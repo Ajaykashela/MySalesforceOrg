1dbfde3f15cf9832909e5aaad4d6ad8b9656893e
Conga_akashela <akashela@conga.com>
2025-04-23 12:14:29 +0530

    LWC changed

--- force-app/main/default/lwc/compA/compA.js
+++ force-app/main/default/lwc/compA/compA.js
@@ -3,7 +3,7 @@ import { LightningElement } from 'lwc';
 export default class CompA extends LightningElement {
 
     calledFromParent(){
-        console.log('request about to be sent from parent')
+        console.log('changed request about to be sent from parent')
         this.refs.compb.calledFromParent();
     }
     handleparent(){
